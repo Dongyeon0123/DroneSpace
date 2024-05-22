8dc0adea7cf2c3af7743004839a8bb31feacd312
dongyeon <ldy1951123@gmail.com>
2024-05-23 05:32:55 +0900

    로그인 페이지 로고 삽입

--- login.php
+++ login.php
@@ -33,12 +33,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_SESSION['loggedin'] = true;
             $_SESSION['memberid'] = $memberid;
             echo '<script>
-                alert("로그인이 되었습니다.");
+                alert("로그인 되었습니다.");
                 window.location.href = "main.php";
                 </script>';
         } else {
             echo '<script>
-                alert("비밀번호가 틀렸습니다.");
+                alert("비밀번호가 일치하지 않습니다..");
                 window.history.back();
                 </script>';
         }
